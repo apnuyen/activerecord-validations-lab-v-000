1d076d85cbd15c679b4f5c7408936333bd36dfc3
An Nuyen <annuyen@gmail.com>
2018-03-08 19:30:10 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
-  validates :category, inclusions: {in: %w(Fiction Non-Fiction)}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
   validates :is_clickbait?
 
   def is_clickbait?
